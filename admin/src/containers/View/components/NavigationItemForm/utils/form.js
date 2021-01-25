7ce64851c9280eddf371f8902e6db4a8952cfc0d
Maksymilian Pamuła <maksymilian.pamula@gmail.com>
2021-01-25 17:31:29 +0100

    feat: single content type support - small fixes

--- admin/src/containers/View/components/NavigationItemForm/utils/form.js
+++ admin/src/containers/View/components/NavigationItemForm/utils/form.js
@@ -7,37 +7,39 @@ import pluginId from "../../../../../pluginId";
 export const form = {
   fieldsToDisable: [],
   fieldsToOmit: [],
-  schema: yup.object({
-    title: yup.string().required(translatedErrors.required),
-    uiRouterKey: yup.string().required(translatedErrors.required),
-    type: yup.string().required(translatedErrors.required),
-    path: yup.string()
-      .when('type', {
-        is: val => val === navigationItemType.INTERNAL || isNil(val),
-        then: yup.string().required(translatedErrors.required),
-        otherwise: yup.string().notRequired(),
-      }),
-    externalPath: yup.string()
-      .when('type', {
-        is: val => val === navigationItemType.EXTERNAL,
-        then: yup.string()
-          .required(translatedErrors.required)
-          .url(`${pluginId}.popup.item.form.externalPath.validation.type`),
-        otherwise: yup.string().notRequired(),
-      }),
-    menuAttached: yup.boolean(),
-    relatedType: yup.mixed()
-      .when('type', {
-        is: val => val === navigationItemType.INTERNAL || isNil(val),
-        then: yup.mixed().required(translatedErrors.required),
-        otherwise: yup.mixed().notRequired(),
-      }),
-    related: yup.mixed()
-      .when('type', {
-        is: val => val === navigationItemType.INTERNAL || isNil(val),
-        then: yup.mixed().required(translatedErrors.required),
-        otherwise: yup.mixed().notRequired(),
-      }),
-  }),
-  inputsPrefix: "",
+  schema(isSingleSelected) {
+    return yup.object({
+      title: yup.string().required(translatedErrors.required),
+      uiRouterKey: yup.string().required(translatedErrors.required),
+      type: yup.string().required(translatedErrors.required),
+      path: yup.string()
+        .when('type', {
+          is: val => val === navigationItemType.INTERNAL || isNil(val),
+          then: yup.string().required(translatedErrors.required),
+          otherwise: yup.string().notRequired(),
+        }),
+      externalPath: yup.string()
+        .when('type', {
+          is: val => val === navigationItemType.EXTERNAL,
+          then: yup.string()
+            .required(translatedErrors.required)
+            .url(`${pluginId}.popup.item.form.externalPath.validation.type`),
+          otherwise: yup.string().notRequired(),
+        }),
+      menuAttached: yup.boolean(),
+      relatedType: yup.mixed()
+        .when('type', {
+          is: val => val === navigationItemType.INTERNAL || isNil(val),
+          then: isSingleSelected ? yup.mixed().notRequired() : yup.mixed().required(translatedErrors.required),
+          otherwise: yup.mixed().notRequired(),
+        }),
+      related: yup.mixed()
+        .when('type', {
+          is: val => val === navigationItemType.INTERNAL || isNil(val),
+          then: isSingleSelected ? yup.mixed().notRequired() : yup.mixed().required(translatedErrors.required),
+          otherwise: yup.mixed().notRequired(),
+        }),
+    });
+  },
+  inputsPrefix: '',
 };
