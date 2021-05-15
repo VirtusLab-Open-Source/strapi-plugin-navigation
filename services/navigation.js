f359b2f3b1864e4d1d11046d51912c9a750a88bd
Eddie Li <lh.eddieli@gmail.com>
2021-05-15 19:42:33 -0400

    use model name is enough for both manually created content type and plugin created

--- services/navigation.js
+++ services/navigation.js
@@ -124,15 +124,14 @@ module.exports = {
       .map(({ key, available}) => {
         const item = strapi.contentTypes[key];
         const relatedField = (item.associations || []).find(_ => _.model === 'navigationitem');
-        const { uid, options, info, collectionName, modelName, apiName, plugin, kind } = item;
+        const { uid, options, info, collectionName, modelName, plugin, kind } = item;
         const { name, description } = info;
         const { isManaged, hidden, templateName } = options;
         const isSingle = kind === KIND_TYPES.SINGLE;
-        const pluralizeName = apiName ? apiName : modelName;
-        const endpoint = isSingle ? pluralizeName : pluralize(pluralizeName);
-        const relationName = utilsFunctions.singularize(pluralizeName);
+        const endpoint = isSingle ? modelName : pluralize(modelName);
+        const relationName = utilsFunctions.singularize(modelName);
         const relationNameParts = last(uid.split('.')).split('-');
-        const contentTypeName = relationNameParts.length > 1 ? relationNameParts.reduce((prev, curr) => `${prev}${upperFirst(curr)}`, '') : upperFirst(pluralizeName);
+        const contentTypeName = relationNameParts.length > 1 ? relationNameParts.reduce((prev, curr) => `${prev}${upperFirst(curr)}`, '') : upperFirst(modelName);
         const labelSingular = name || (upperFirst(relationNameParts.length > 1 ? relationNameParts.join(' ') : relationName));
         return {
           uid,
