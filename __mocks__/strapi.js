75ccdde05ba5215646b138752dba76ead121ec09
Maciej Witkowski <maciekwitkowski322@gmail.com>
2022-02-03 10:12:07 +0100

    fix: tests coverage

---
+++ __mocks__/strapi.js
@@ -0,0 +1,196 @@
+const { isMatch } = require('lodash');
+
+const masterModelMock = {
+    findOne: () => ({
+        id: 1,
+        name: "Main navigation",
+        slug: "main-navigation",
+        visible: true,
+        createdAt: "2021-12-30T14:05:50.276Z",
+        updatedAt: "2021-12-30T14:05:50.276Z",
+    }),
+    findMany: () => [{
+        id: 1,
+        name: "Main navigation",
+        slug: "main-navigation",
+        visible: true,
+        createdAt: "2021-12-30T14:05:50.276Z",
+        updatedAt: "2021-12-30T14:05:50.276Z",
+    }],
+};
+
+const itemModelMock = {
+    findOne: async () => ({
+        id: 1,
+        title: "home",
+        type: "INTERNAL",
+        path: "home1",
+        externalPath: null,
+        uiRouterKey: "home",
+        menuAttached: true,
+        order: 1,
+        createdAt: "2021-12-31T10:04:54.812Z",
+        updatedAt: "2022-01-14T13:36:29.430Z",
+        related: {
+            id: 1,
+            related_id: "1",
+            related_type: "api::pages.pages",
+            field: "navigation",
+            order: 1,
+            master: "3",
+            createdAt: "2021-12-31T10:04:54.800Z",
+            updatedAt: "2021-12-31T10:04:54.800Z",
+            navigationItemId: 56,
+        },
+        parent: null,
+    }),
+    findMany: async ({where}) => [{
+        id: 1,
+        title: "home",
+        type: "INTERNAL",
+        path: "home1",
+        externalPath: null,
+        uiRouterKey: "home",
+        menuAttached: true,
+        order: 1,
+        createdAt: "2021-12-31T10:04:54.812Z",
+        updatedAt: "2022-01-14T13:36:29.430Z",
+        master: 1, 
+        related: {
+            id: 1,
+            related_id: "1",
+            related_type: "api::pages.pages",
+            field: "navigation",
+            order: 1,
+            master: "3",
+            createdAt: "2021-12-31T10:04:54.800Z",
+            updatedAt: "2021-12-31T10:04:54.800Z",
+            navigationItemId: 56,
+        },
+        parent: null,
+    }, {
+        id: 2,
+        title: "side",
+        type: "INTERNAL",
+        path: "side",
+        externalPath: null,
+        uiRouterKey: "side",
+        menuAttached: false,
+        order: 1,
+        createdAt: "2021-12-31T10:04:54.824Z",
+        updatedAt: "2021-12-31T12:47:20.508Z",
+        master: 1,
+        related: {
+            id: 2,
+            related_id: "2",
+            related_type: "api::pages.pages",
+            field: "navigation",
+            order: 1,
+            master: "3",
+            createdAt: "2021-12-31T10:04:54.823Z",
+            updatedAt: "2021-12-31T10:04:54.823Z",
+            navigationItemId: 57,
+        },
+        parent: {
+            id: 1,
+            title: "home",
+            type: "INTERNAL",
+            path: "home1",
+            externalPath: null,
+            uiRouterKey: "home",
+            menuAttached: true,
+            order: 1,
+            createdAt: "2021-12-31T10:04:54.812Z",
+            updatedAt: "2022-01-14T13:36:29.430Z",
+        },
+    }].filter(item => isMatch(item, where)),
+};
+
+const pageModelMock = {
+    findOne: async ({ where }) => ({
+        "id": 1,
+        "attributes": {
+            "title": "Page nr 1",
+            "createdAt": "2022-01-19T08:22:31.244Z",
+            "updatedAt": "2022-01-19T08:22:31.244Z",
+            "publishedAt": null
+        }
+    }),
+    findMany: async ({ where }) => [{
+            "id": 1,
+            "attributes": {
+                "title": "Page nr 1",
+                "createdAt": "2022-01-19T08:22:31.244Z",
+                "updatedAt": "2022-01-19T08:22:31.244Z",
+                "publishedAt": null
+            }
+        }, {
+            "id": 2,
+            "attributes": {
+                "title": "Page nr 2",
+                "createdAt": "2022-01-19T08:22:50.821Z",
+                "updatedAt": "2022-01-19T08:22:50.821Z",
+                "publishedAt": null
+            }
+        }]
+
+};
+
+const plugins = (strapi) => ({
+    navigation: {
+        get services() { return require('../server/services') },
+        service: (key) => (require('../server/services'))[key]({ strapi }),
+        get contentTypes() { return require('../server/content-types') },
+        contentType: (key) => preparePluginContentType(require('../server/content-types')[key].schema, 'navigation'),
+        config: (key) => ({
+            ...require('../server/config').default,
+            contentTypes: ['api::pages.pages'],
+        })[key],
+    }
+});
+
+const contentTypes = {
+    'api::pages.pages': {
+        ...require('./pages.settings.json'),
+        uid: 'api::pages.pages',
+        modelName: 'page',
+    },
+};
+
+const preparePluginContentType = (schema, plugin) => {
+    const { name } = schema.info;
+
+    return {
+        ...schema,
+        uid: `plugin::${plugin}.${name}`,
+        modelName: name,
+    }
+}
+
+const strapiFactory = (plugins, contentTypes) => ({
+    get plugins() { return plugins(strapi) },
+    plugin: (name) => plugins(strapi)[name],
+    get contentTypes() { return contentTypes },
+    contentType: (key) => contentTypes[key],
+    query: (model) => {
+        switch (model) {
+            case 'plugin::navigation.navigation':
+                return masterModelMock;
+            case 'plugin::navigation.navigation-item':
+                return itemModelMock;
+            case 'api::pages.pages':
+                return pageModelMock;
+            default:
+                return {
+                    findOne: () => ({}),
+                    findMany: () => [],
+                }
+        }
+    }
+});
+
+const setupStrapi = () => {
+    Object.defineProperty(global, 'strapi', { value: strapiFactory(plugins, contentTypes) });
+}
+
+module.exports = { setupStrapi };
--- server/services/__tests__/navigation.test.js
+++ server/services/__tests__/navigation.test.js
@@ -1,84 +1,69 @@
-const { setupStrapi } = require('../../__mocks__/helpers/strapi');
+const { setupStrapi } = require('../../../__mocks__/strapi');
 
-beforeAll(setupStrapi);
-
-describe('Navigation service', () => {
-  it('Strapi is defined', () => {
-    expect(strapi).toBeDefined();
-    expect(strapi.contentTypes).toBeDefined();
-    expect(Object.keys(strapi.contentTypes).length).toBe(6);
+describe('Navigation services', () => {
+  beforeAll(async () => {
+    setupStrapi();
   });
-  it('Config Content Types', () => {
-    const { configContentTypes } = require('../navigation');
-    const results = [
-      {
-        uid: 'application::pages.pages',
-        collectionName: 'pages',
-        isSingle: false,
-        contentTypeName: 'Pages',
-        endpoint: 'pages',
-        label: 'Pages',
-        labelSingular: 'Page',
-        name: 'page',
-        visible: true,
-      }, {
-        uid: 'application::blog-post.blog-post',
-        collectionName: 'blog_posts',
-        isSingle: false,
-        contentTypeName: 'BlogPost',
-        endpoint: 'blog-posts',
-        label: 'Blog posts',
-        labelSingular: 'Blog post',
-        name: 'blog-post',
-        visible: true,
-      }, {
-        uid: 'application::my-homepages.my-homepage',
-        collectionName: 'my-homepages',
-        isSingle: true,
-        contentTypeName: 'MyHomepage',
-        endpoint: 'my-homepage',
-        label: 'My Homepage',
-        labelSingular: 'My Homepage',
-        name: 'my-homepage',
-        visible: true,
-      }, {
-        uid: 'application::page-homes.home-page',
-        collectionName: 'page_homes',
-        isSingle: true,
-        contentTypeName: 'HomePage',
-        endpoint: 'custom-api',
-        label: 'Page Home',
-        labelSingular: 'Page Home',
-        name: 'home-page',
-        visible: true,
-      }, {
-        uid: 'plugins::another-plugin.pages',
-        collectionName: 'pages',
-        isSingle: false,
-        contentTypeName: 'Plugin-pages',
-        endpoint: 'plugin-pages',
-        label: 'Pages',
-        labelSingular: 'Page',
-        name: 'plugin-page',
-        visible: true,
-        plugin: 'another-plugin',
-      }, {
-        uid: 'plugins::another-plugin.blog-post',
-        collectionName: 'blog_posts',
-        isSingle: false,
-        contentTypeName: 'BlogPost',
-        endpoint: 'plugin-blog-posts',
-        label: 'Blog posts',
-        labelSingular: 'Blog post',
-        name: 'plugin-blog-post',
-        visible: true,
-        plugin: 'another-plugin',
-      }];
-    return configContentTypes().then(types => {
-      types.map(type => {
-        const result = results.find(({ uid }) => uid === type.uid);
-        expect(type).toMatchObject(result);
-      });
+
+  describe('Correct config', () => {
+    it('Declares Strapi instance', () => {
+      expect(strapi).toBeDefined()
+      expect(strapi.plugin('navigation').service('navigation')).toBeDefined()
+    });
+
+    it('Defines proper content types', () => {
+      expect(strapi.contentTypes).toBeDefined()
+      expect(strapi.plugin('navigation').contentTypes).toBeDefined()
+    });
+
+    it('Can read and return plugins config', () => {
+      expect(strapi.plugin('navigation').config('contentTypes')).toBeDefined()
+      expect(strapi.plugin('navigation').config('allowedLevels')).toBeDefined()
+      expect(strapi.plugin('navigation').config()).not.toBeDefined()
+    });
+  });
+
+  describe('Render navigation', () => {
+    it('Can render branch in flat format', async () => {
+      const service = strapi.plugin('navigation').service('navigation');
+      const result = await service.render(1);
+
+      expect(result).toBeDefined()
+      expect(result.length).toBe(2)
+    });
+
+    it('Can render branch in tree format', async () => {
+      const service = strapi.plugin('navigation').service('navigation');
+      const result = await service.render(1, "TREE");
+
+      expect(result).toBeDefined()
+      expect(result.length).toBeGreaterThan(0)
+    });
+
+    it('Can render branch in rfr format', async () => {
+      const service = strapi.plugin('navigation').service('navigation');
+      const result = await service.render(1, "RFR");
+
+      expect(result).toBeDefined()
+      expect(result.length).toBeGreaterThan(0)
+    });
+
+    it('Can render only menu attached elements', async () => {
+      const service = strapi.plugin('navigation').service('navigation');
+      const result = await service.render(1, "FLAT", true);
+
+      expect(result).toBeDefined()
+      expect(result.length).toBe(1)
+    });
+  });
+
+  describe('Render child', () => {
+    it('Can render child', async () => {
+      const service = strapi.plugin('navigation').service('navigation');
+      const result = await service.renderChildren(1, "home");
+
+      expect(result).toBeDefined();
+      expect(result.length).toBe(1);
     });
   });
 });
