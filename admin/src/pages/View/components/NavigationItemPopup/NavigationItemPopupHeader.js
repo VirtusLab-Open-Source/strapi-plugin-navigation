54f487c8f8d20767842ffbc748e159e1e6b38374
Maciej Witkowski <maciekwitkowski322@gmail.com>
2022-03-17 08:57:41 +0100

    fix: design system bug

--- admin/src/pages/View/components/NavigationItemPopup/NavigationItemPopupHeader.js
+++ admin/src/pages/View/components/NavigationItemPopup/NavigationItemPopupHeader.js
@@ -1,16 +1,16 @@
 
 
 import React from 'react';
-import { ButtonText } from '@strapi/design-system/Text';
+import { Typography } from '@strapi/design-system/Typography';
 import { ModalHeader } from '@strapi/design-system/ModalLayout';
 import { getMessage } from '../../../../utils';
 
 export const NavigationItemPopupHeader = ({isNewItem}) => {
 	return (
 		<ModalHeader>
-			<ButtonText textColor="neutral800" as="h2" id="asset-dialog-title">
+			<Typography variant="omega" fontWeight="bold" textColor="neutral800" as="h2" id="asset-dialog-title">
 				{getMessage(`popup.item.header.${isNewItem ? 'new' : 'edit'}`)}
-			</ButtonText>
+			</Typography>
 		</ModalHeader>
 	);
 };
