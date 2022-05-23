bdc3b2706c4c383a37f132cc2bb4295d647d4a51
CodeVoyager <tmsz.dnl.puch@gmail.com>
2022-05-23 14:17:44 +0200

    chore: strapi-typed version bump

--- types/controllers.ts
+++ types/controllers.ts
@@ -1,5 +1,5 @@
-import { StrapiRequestContext } from "strapi-typed";
-import { Navigation } from "./contentTypes";
+import { StrapiController } from "strapi-typed";
+import { Navigation, NotVoid } from "./contentTypes";
 import { IAdminService, IClientService, NavigationService, NavigationServiceName } from "./services";
 import { AuditLogContext, StrapiControllerContext, ToBeFixed } from "./utils";
 
@@ -20,8 +20,12 @@ export interface IAdminController {
   settingsConfig: () => ToBeFixed;
   settingsRestart: (ctx: StrapiControllerContext) => ToBeFixed;
   updateConfig: (ctx: StrapiControllerContext) => ToBeFixed;
-  fillFromOtherLocale: (ctx: StrapiRequestContext<never, never, { source: string, target: string }> & ControllerCommonContext) => Promise<Navigation>;
-  readNavigationItemFromLocale: (ctx: StrapiRequestContext<never, { path?: string }, { source: string, target: string }> & ControllerCommonContext) => Promise<ToBeFixed>;
+  fillFromOtherLocale: StrapiController<Promise<Navigation>, never, never, { source: string, target: string }, ControllerCommonContext>,
+  readNavigationItemFromLocale: StrapiController<Promise<
+    Partial<
+      Pick<NotVoid<Navigation['items']>[number], 'path' | 'related' | 'type' | 'uiRouterKey' | 'title' | 'externalPath'>
+    >
+  >, never, { path?: string }, { source: string, target: string }, ControllerCommonContext>
 };
 
 export interface IClientController {
