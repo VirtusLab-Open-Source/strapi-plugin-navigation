d8f93172475e30598f9b5f0060a69fd100d7a159
sascha94 <sascha-kycia@hotmail.com>
2022-09-19 23:51:04 +0200

    add new settings option 'cascadeMenuAttached'

--- server/config/index.ts
+++ server/config/index.ts
@@ -16,6 +16,7 @@ const config: StrapiConfig<NavigationPluginConfig> = {
     pathDefaultFields: {},
     pruneObsoleteI18nNavigations: false,
     slugify: {},
+    cascadeMenuAttached: true,
   },
 };
 
