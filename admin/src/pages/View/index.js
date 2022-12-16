03b359f8df364eaf76116ed4594ccdd5d89cceb0
Mateusz Ziarko <mziarko@virtuslab.com>
2022-12-16 09:11:38 +0100

    [CU-862hxegk3] Search should be case insensitive

--- admin/src/pages/View/index.js
+++ admin/src/pages/View/index.js
@@ -149,7 +149,7 @@ const View = () => {
     else
       return [...subItems, ...acc];
   }, []);
-  const filteredList = !isSearchEmpty ? filteredListFactory(changedActiveNavigation.items, (item) => item?.title.includes(searchValue)) : [];
+  const filteredList = !isSearchEmpty ? filteredListFactory(changedActiveNavigation.items, (item) => (item?.title || '').toLowerCase().includes(searchValue.toLowerCase())) : [];
 
   const changeCollapseItemDeep = (item, isCollapsed) => {
     if (item.collapsed !== isCollapsed) {
