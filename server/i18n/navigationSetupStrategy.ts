8212fc25225db521f2defff91a08662893914a16
CodeVoyager <tmsz.dnl.puch@gmail.com>
2023-09-20 14:18:16 +0200

    fix: db deadlocks and dup errors

--- server/i18n/navigationSetupStrategy.ts
+++ server/i18n/navigationSetupStrategy.ts
@@ -1,4 +1,4 @@
-import { IStrapi, OnlyStrings } from "strapi-typed";
+import { IStrapi, OnlyStrings, StrapiDBBulkActionResponse } from "strapi-typed";
 import {
   assertEntity,
   assertNotEmpty,
@@ -7,10 +7,9 @@ import {
   Navigation,
   NavigationPluginConfig,
 } from "../../types";
-import {
-  DEFAULT_NAVIGATION_ITEM, DEFAULT_POPULATE,
-} from "../utils";
+import { DEFAULT_NAVIGATION_ITEM, DEFAULT_POPULATE } from "../utils";
 import { DefaultLocaleMissingError } from "./errors";
+import { prop } from "lodash/fp";
 
 export const i18nNavigationSetupStrategy: INavigationSetupStrategy = async ({
   strapi,
@@ -40,67 +39,74 @@ export const i18nNavigationSetupStrategy: INavigationSetupStrategy = async ({
       ];
     }
 
-    if (currentNavigations.some(hasNotAnyLocale)) {
-      currentNavigations = await Promise.all(
-        currentNavigations.map(async (navigation) => {
-          return hasNotAnyLocale(navigation)
-            ? await updateNavigation({
-                strapi,
-                current: navigation,
-                payload: {
-                  localeCode: defaultLocale,
-                },
-                populate: DEFAULT_POPULATE,
-              })
-            : navigation;
-        })
-      );
-    }
+    const noLocaleNavigations = currentNavigations.filter(hasNotAnyLocale);
 
-    await Promise.all(
-      currentNavigations
-        .filter(({ localeCode }) => defaultLocale === localeCode)
-        .flatMap(async (rootNavigation) => {
-          const localizations = [
-            ...(rootNavigation.localizations ?? []).map<Navigation>(
-              (localization) => assertEntity(localization, "Navigation")
-            ),
-            rootNavigation,
-          ];
-
-          for (const locale of allLocale) {
-            if (
-              !localizations.some(({ localeCode }) => localeCode === locale)
-            ) {
-              localizations.push(
-                await createNavigation({
-                  strapi,
-                  payload: {
-                    localeCode: locale,
-                    slug: `${rootNavigation.slug}-${locale}`,
-                    name: rootNavigation.name,
-                    visible: true,
-                  },
-                })
-              );
-            }
+    if (noLocaleNavigations.length) {
+      await updateNavigations({
+        strapi,
+        ids: noLocaleNavigations.map(prop("id")).reduce((acc, id) => {
+          if (id && Number(id)) {
+            acc.push(Number(id));
           }
 
-          return await Promise.all(
-            localizations.map((current) =>
-              updateNavigation({
-                strapi,
-                current,
-                payload: {
-                  localizations: localizations.filter(
-                    ({ id }) => id !== current.id
-                  ),
-                },
-              })
-            )
-          );
-        })
+          return acc;
+        }, [] as Array<number>),
+        payload: {
+          localeCode: defaultLocale,
+        },
+        populate: DEFAULT_POPULATE,
+      });
+
+      currentNavigations = await getCurrentNavigations(strapi);
+    }
+
+    const navigationsToProcess = currentNavigations.filter(
+      ({ localeCode }) => defaultLocale === localeCode
     );
+
+    for (const rootNavigation of navigationsToProcess) {
+      let localizations = [
+        ...(rootNavigation.localizations ?? []).map<Navigation>(
+          (localization) => assertEntity(localization, "Navigation")
+        ),
+        rootNavigation,
+      ];
+
+      const missingLocale = allLocale.filter(
+        (locale) =>
+          !localizations.some(({ localeCode }) => localeCode === locale)
+      );
+
+      if (missingLocale.length) {
+        const { ids } = await createNavigations({
+          strapi,
+          payloads: missingLocale.map((locale) => ({
+            localeCode: locale,
+            slug: `${rootNavigation.slug}-${locale}`,
+            name: rootNavigation.name,
+            visible: true,
+          })),
+        });
+
+        localizations = [...(await getCurrentNavigations(strapi, ids))].concat([
+          rootNavigation,
+        ]);
+      }
+
+      // TODO: Update to bulk operation when strapi
+      // allows to update `oneToMany` relations on bulk update
+      for (const current of localizations) {
+        await updateNavigation({
+          strapi,
+          current,
+          payload: {
+            localizations: localizations.filter(
+              (localization) => localization.id !== current.id
+            ),
+          },
+        });
+      }
+    }
   } else {
     if (config.pruneObsoleteI18nNavigations) {
       await deleteNavigations({
@@ -125,8 +131,11 @@ export const i18nNavigationSetupStrategy: INavigationSetupStrategy = async ({
   return getCurrentNavigations(strapi);
 };
 
-const getCurrentNavigations = (strapi: IStrapi): Promise<Navigation[]> =>
-  strapi.plugin("navigation").service("admin").get();
+const getCurrentNavigations = (
+  strapi: IStrapi,
+  ids?: Array<number>
+): Promise<Navigation[]> =>
+  strapi.plugin("navigation").service("admin").get(ids);
 
 // TODO: Move to service
 const createNavigation = ({
@@ -145,7 +154,21 @@ const createNavigation = ({
     populate,
   });
 
-// TODO: Move to service
+// TODO: update strapi-typed
+const createNavigations = ({
+  strapi,
+  payloads,
+  populate,
+}: {
+  strapi: IStrapi;
+  payloads: Array<Partial<Navigation>>;
+  populate?: Array<keyof Navigation>;
+}): Promise<any> =>
+  strapi.query<Navigation>("plugin::navigation.navigation").createMany({
+    data: payloads,
+    populate,
+  });
+
 const updateNavigation = ({
   strapi,
   current,
@@ -167,6 +190,27 @@ const updateNavigation = ({
     },
   });
 
+const updateNavigations = ({
+  strapi,
+  ids,
+  payload,
+  populate,
+}: {
+  strapi: IStrapi;
+  payload: Partial<Navigation>;
+  ids: Array<number>;
+  populate?: Array<OnlyStrings<keyof Navigation>>;
+}): Promise<StrapiDBBulkActionResponse> =>
+  strapi.query<Navigation>("plugin::navigation.navigation").updateMany({
+    data: payload,
+    populate,
+    where: {
+      id: {
+        $in: ids,
+      },
+    },
+  });
+
 // TODO: Move to service
 const deleteNavigations = ({
   strapi,
@@ -179,14 +223,18 @@ const deleteNavigations = ({
     where,
   });
 
-const createDefaultI18nNavigation = ({ strapi, defaultLocale }: {
+const createDefaultI18nNavigation = ({
+  strapi,
+  defaultLocale,
+}: {
   strapi: IStrapi;
   defaultLocale: string;
-}): Promise<Navigation> => createNavigation({
-  strapi,
-  payload: {
-    ...DEFAULT_NAVIGATION_ITEM,
-    localeCode: defaultLocale,
-  },
-  populate: DEFAULT_POPULATE,
-});
+}): Promise<Navigation> =>
+  createNavigation({
+    strapi,
+    payload: {
+      ...DEFAULT_NAVIGATION_ITEM,
+      localeCode: defaultLocale,
+    },
+    populate: DEFAULT_POPULATE,
+  });
