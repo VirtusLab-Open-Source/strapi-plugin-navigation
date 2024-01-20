d74aa09acf04a2d3df4ba2065ac430965dbaf85f
CodeVoyager <tmsz.dnl.puch@gmail.com>
2024-01-20 11:33:01 +0100

    chore: client service tests

--- server/services/__tests__/service.test.ts
+++ server/services/__tests__/service.test.ts
@@ -1,36 +1,43 @@
-import { IStrapi } from "strapi-typed";
-import { ToBeFixed } from "../../../types";
+import { IStrapi, StrapiContext } from "strapi-typed";
+import { Navigation, ToBeFixed } from "../../../types";
 
 import setupStrapi from '../../../__mocks__/strapi';
 import { allLifecycleHooks, getPluginService, RENDER_TYPES } from "../../utils";
+import clientService from "../client";
 
-declare var strapi: IStrapi
+declare var strapi: IStrapi;
 
-describe('Navigation services', () => {
+describe("Navigation services", () => {
   beforeAll(async () => {
     setupStrapi();
   });
 
-  describe('Correct config', () => {
-    it('Declares Strapi instance', () => {
-      expect(strapi).toBeDefined()
-      expect(strapi.plugin('navigation').service('admin')).toBeDefined()
-      expect(strapi.plugin('navigation').service('client')).toBeDefined()
-      expect(strapi.plugin('navigation').service('common')).toBeDefined()
+  describe("Correct config", () => {
+    it("Declares Strapi instance", () => {
+      expect(strapi).toBeDefined();
+      expect(strapi.plugin("navigation").service("admin")).toBeDefined();
+      expect(strapi.plugin("navigation").service("client")).toBeDefined();
+      expect(strapi.plugin("navigation").service("common")).toBeDefined();
     });
 
-    it('Defines proper content types', () => {
-      expect(strapi.contentTypes).toBeDefined()
-      expect(strapi.plugin('navigation').contentTypes).toBeDefined()
+    it("Defines proper content types", () => {
+      expect(strapi.contentTypes).toBeDefined();
+      expect(strapi.plugin("navigation").contentTypes).toBeDefined();
     });
 
-    it('Can read and return plugins config', () => {
-      expect(strapi.plugin('navigation').config('additionalFields')).toBeDefined()
-      expect(strapi.plugin('navigation').config('contentTypes')).toBeDefined()
-      expect(strapi.plugin('navigation').config('contentTypesNameFields')).toBeDefined()
-      expect(strapi.plugin('navigation').config('contentTypesPopulate')).toBeDefined()
-      expect(strapi.plugin('navigation').config('allowedLevels')).toBeDefined()
-      expect(strapi.plugin('navigation').config('gql')).toBeDefined()
+    it("Can read and return plugins config", () => {
+      expect(
+        strapi.plugin("navigation").config("additionalFields")
+      ).toBeDefined();
+      expect(strapi.plugin("navigation").config("contentTypes")).toBeDefined();
+      expect(
+        strapi.plugin("navigation").config("contentTypesNameFields")
+      ).toBeDefined();
+      expect(
+        strapi.plugin("navigation").config("contentTypesPopulate")
+      ).toBeDefined();
+      expect(strapi.plugin("navigation").config("allowedLevels")).toBeDefined();
+      expect(strapi.plugin("navigation").config("gql")).toBeDefined();
     });
   });
 
@@ -52,23 +59,49 @@ describe('Navigation services', () => {
 
       expect(result).toBeDefined();
       expect(result.length).toBe(2);
-      expect(result).toHaveProperty([0, 'related', 'id'], 1);
-      expect(result).toHaveProperty([0, 'related', 'attributes', 'title'], 'Page nr 1');
-      expect(result).toHaveProperty([0, 'string_test_field'], 'Custom field value');
+      expect(result).toHaveProperty([0, "related", "id"], 1);
+      expect(result).toHaveProperty([0, "related", "title"], "Page nr 1");
+      expect(result).toHaveProperty(
+        [0, "string_test_field"],
+        "Custom field value"
+      );
     });
 
     it('Can render branch in tree format', async () => {
       const clientService = getPluginService('client');
       const result = await clientService.render({
         idOrSlug: 1,
-        type: RENDER_TYPES.TREE
+        wrapRelated: true,
+      });
+
+      expect(result).toBeDefined();
+      expect(result.length).toBe(2);
+      expect(result).toHaveProperty([0, "related", "id"], 1);
+      expect(result).toHaveProperty(
+        [0, "related", "attributes", "title"],
+        "Page nr 1"
+      );
+      expect(result).toHaveProperty(
+        [0, "string_test_field"],
+        "Custom field value"
+      );
+    });
+
+    it("Can render branch in tree format", async () => {
+      const clientService = getPluginService("client");
+      const result = await clientService.render({
+        idOrSlug: 1,
+        type: RENDER_TYPES.TREE,
       });
 
       expect(result).toBeDefined();
       expect(result.length).toBeGreaterThan(0);
       expect(result[0].items).toBeDefined();
       expect(result[0].items.length).toBeGreaterThan(0);
-      expect(result).toHaveProperty([0, 'items', 0, 'string_test_field'], 'Custom field value');
+      expect(result).toHaveProperty(
+        [0, "items", 0, "string_test_field"],
+        "Custom field value"
+      );
     });
 
     it('Can render branch in tree format for GraphQL', async () => {
@@ -81,23 +114,32 @@ describe('Navigation services', () => {
 
       expect(result).toBeDefined();
       expect(result.length).toBeGreaterThan(0);
-      expect(result).toHaveProperty([0, 'related', 'id'], 1);
-      expect(result).toHaveProperty([0, 'related', 'attributes', 'title'], 'Page nr 1');
-      expect(result).toHaveProperty([0, 'items', 0, 'related', 'id'], 2);
-      expect(result).toHaveProperty([0, 'items', 0, 'related', 'attributes', 'title'], 'Page nr 2');
-      expect(result).toHaveProperty([0, 'string_test_field'], 'Custom field value');
+      expect(result).toHaveProperty([0, "related", "id"], 1);
+      expect(result).toHaveProperty(
+        [0, "related", "attributes", "title"],
+        "Page nr 1"
+      );
+      expect(result).toHaveProperty([0, "items", 0, "related", "id"], 2);
+      expect(result).toHaveProperty(
+        [0, "items", 0, "related", "attributes", "title"],
+        "Page nr 2"
+      );
+      expect(result).toHaveProperty(
+        [0, "string_test_field"],
+        "Custom field value"
+      );
     });
 
     it('Can render branch in rfr format', async () => {
       const clientService = getPluginService('client');
       const result = await clientService.render({
         idOrSlug: 1,
-        type: RENDER_TYPES.RFR
+        type: RENDER_TYPES.RFR,
       });
 
-      expect(result).toBeDefined()
-      expect(result.pages).toBeDefined()
-      expect(result.nav).toBeDefined()
+      expect(result).toBeDefined();
+      expect(result.pages).toBeDefined();
+      expect(result.nav).toBeDefined();
     });
 
     it('Can render branch in rfr format for GraphQL', async () => {
@@ -111,7 +153,7 @@ describe('Navigation services', () => {
       expect(result).toBeDefined();
       expect(result.pages).toBeDefined();
       expect(result.nav).toBeDefined();
-      expect(result).toHaveProperty(['pages', 'home', 'related', 'id'], 1);
+      expect(result).toHaveProperty(["pages", "home", "related", "id"], 1);
     });
 
     it('Can render only menu attached elements', async () => {
@@ -122,8 +164,8 @@ describe('Navigation services', () => {
         menuOnly: true,
       });
 
-      expect(result).toBeDefined()
-      expect(result.length).toBe(1)
+      expect(result).toBeDefined();
+      expect(result.length).toBe(1);
     });
 
     it('Can render branch by path', async () => {
@@ -132,7 +174,7 @@ describe('Navigation services', () => {
         idOrSlug: 1,
         type: RENDER_TYPES.FLAT,
         menuOnly: false,
-        rootPath: '/home/side'
+        rootPath: "/home/side",
       });
 
       expect(result).toBeDefined();
@@ -143,7 +185,7 @@ describe('Navigation services', () => {
   describe('Render child', () => {
     it('Can render child', async () => {
       const clientService = getPluginService('client');
-      const result = await clientService.renderChildren({idOrSlug: 1, childUIKey: "home"});
+      const result = await clientService.renderChildren({ idOrSlug: 1, childUIKey: "home" });
 
       expect(result).toBeDefined();
       expect(result.length).toBe(1);
@@ -181,4 +223,316 @@ describe('Navigation services', () => {
       expect(listenerB).toHaveBeenCalledTimes(1);
     })
   })
+  describe("ClientService", () => {
+    let index = 0;
+    const generateNavigation = (
+      rest: Partial<Navigation> = {}
+    ): Partial<Navigation> => ({
+      name: `Navigation-${++index}`,
+      id: ++index,
+      ...rest,
+    });
+
+    describe("readAll()", () => {
+      it("should read results", async () => {
+        // Given
+        const allLocale = ["en", "pl", "ff", "fr"];
+        const activeLocale = allLocale.filter((locale) => locale != "fr");
+        const locale = allLocale[0];
+        const findMany = jest.fn();
+        const query = (): any => ({ findMany });
+        const i18nPluginService = {
+          getDefaultLocale() {
+            return locale;
+          },
+          find() {
+            return activeLocale.map((code) => ({ code }));
+          },
+        } as ToBeFixed;
+        const i18nPlugin = {
+          service() {
+            return i18nPluginService;
+          },
+        };
+        const store = () =>
+          ({
+            get() {
+              return { i18nEnabled: false };
+            },
+          } as ToBeFixed);
+        const strapi: Partial<StrapiContext["strapi"]> = {
+          query,
+          plugin(name): any {
+            return name === "i18n" ? i18nPlugin : null;
+          },
+          store,
+        };
+        const navigations = allLocale.map((localeCode) =>
+          generateNavigation({
+            localeCode,
+            localizations: allLocale
+              .filter((locale) => locale !== localeCode)
+              .map((_localeCode) =>
+                generateNavigation({ localeCode: _localeCode })
+              ) as ToBeFixed,
+          })
+        );
+        const service = clientService({
+          strapi: strapi as StrapiContext["strapi"],
+        });
+        const orderBy = "name";
+        const orderDirection = "ASC";
+
+        findMany.mockResolvedValue(navigations);
+
+        // When
+        const result = await service.readAll({
+          orderBy,
+          locale,
+          orderDirection,
+        });
+
+        // Then
+        expect(findMany.mock.calls).toMatchInlineSnapshot(`
+          Array [
+            Array [
+              Object {
+                "limit": 9007199254740991,
+                "orderBy": Object {
+                  "name": "ASC",
+                },
+                "populate": false,
+                "where": Object {
+                  "localeCode": "en",
+                },
+              },
+            ],
+          ]
+        `);
+        expect(result).toMatchInlineSnapshot(`
+          Array [
+            Object {
+              "id": 8,
+              "localeCode": "en",
+              "localizations": Array [
+                Object {
+                  "id": 2,
+                  "localeCode": "pl",
+                  "name": "Navigation-1",
+                },
+                Object {
+                  "id": 4,
+                  "localeCode": "ff",
+                  "name": "Navigation-3",
+                },
+                Object {
+                  "id": 6,
+                  "localeCode": "fr",
+                  "name": "Navigation-5",
+                },
+              ],
+              "name": "Navigation-7",
+            },
+            Object {
+              "id": 16,
+              "localeCode": "pl",
+              "localizations": Array [
+                Object {
+                  "id": 10,
+                  "localeCode": "en",
+                  "name": "Navigation-9",
+                },
+                Object {
+                  "id": 12,
+                  "localeCode": "ff",
+                  "name": "Navigation-11",
+                },
+                Object {
+                  "id": 14,
+                  "localeCode": "fr",
+                  "name": "Navigation-13",
+                },
+              ],
+              "name": "Navigation-15",
+            },
+            Object {
+              "id": 24,
+              "localeCode": "ff",
+              "localizations": Array [
+                Object {
+                  "id": 18,
+                  "localeCode": "en",
+                  "name": "Navigation-17",
+                },
+                Object {
+                  "id": 20,
+                  "localeCode": "pl",
+                  "name": "Navigation-19",
+                },
+                Object {
+                  "id": 22,
+                  "localeCode": "fr",
+                  "name": "Navigation-21",
+                },
+              ],
+              "name": "Navigation-23",
+            },
+            Object {
+              "id": 32,
+              "localeCode": "fr",
+              "localizations": Array [
+                Object {
+                  "id": 26,
+                  "localeCode": "en",
+                  "name": "Navigation-25",
+                },
+                Object {
+                  "id": 28,
+                  "localeCode": "pl",
+                  "name": "Navigation-27",
+                },
+                Object {
+                  "id": 30,
+                  "localeCode": "ff",
+                  "name": "Navigation-29",
+                },
+              ],
+              "name": "Navigation-31",
+            },
+          ]
+        `);
+      });
+      it("should read locale aware results", async () => {
+        // Given
+        const allLocale = ["en", "pl", "ff", "fr"];
+        const activeLocale = allLocale.filter((locale) => locale != "fr");
+        const locale = allLocale[0];
+        const findMany = jest.fn();
+        const query = (): any => ({ findMany });
+        const i18nPluginService = {
+          getDefaultLocale() {
+            return locale;
+          },
+          find() {
+            return activeLocale.map((code) => ({ code }));
+          },
+        } as ToBeFixed;
+        const i18nPlugin = {
+          service() {
+            return i18nPluginService;
+          },
+        };
+        const store = () =>
+          ({
+            get() {
+              return { i18nEnabled: true };
+            },
+          } as ToBeFixed);
+        const strapi: Partial<StrapiContext["strapi"]> = {
+          query,
+          plugin(name): any {
+            return name === "i18n" ? i18nPlugin : null;
+          },
+          store,
+        };
+        const navigations = allLocale.map((localeCode) =>
+          generateNavigation({
+            localeCode,
+            localizations: allLocale
+              .filter((locale) => locale !== localeCode)
+              .map((_localeCode) =>
+                generateNavigation({ localeCode: _localeCode })
+              ) as ToBeFixed,
+          })
+        );
+        const service = clientService({
+          strapi: strapi as StrapiContext["strapi"],
+        });
+        const orderBy = "name";
+        const orderDirection = "ASC";
+
+        findMany.mockResolvedValue(navigations);
+
+        // When
+        const result = await service.readAll({
+          orderBy,
+          locale,
+          orderDirection,
+        });
+
+        // Then
+        expect(findMany.mock.calls).toMatchInlineSnapshot(`
+          Array [
+            Array [
+              Object {
+                "limit": 9007199254740991,
+                "orderBy": Object {
+                  "name": "ASC",
+                },
+                "populate": false,
+                "where": Object {
+                  "localeCode": "en",
+                },
+              },
+            ],
+          ]
+        `);
+        expect(result).toMatchInlineSnapshot(`
+          Array [
+            Object {
+              "id": 40,
+              "localeCode": "en",
+              "localizations": Array [
+                Object {
+                  "id": 34,
+                  "localeCode": "pl",
+                  "name": "Navigation-33",
+                },
+                Object {
+                  "id": 36,
+                  "localeCode": "ff",
+                  "name": "Navigation-35",
+                },
+              ],
+              "name": "Navigation-39",
+            },
+            Object {
+              "id": 48,
+              "localeCode": "pl",
+              "localizations": Array [
+                Object {
+                  "id": 42,
+                  "localeCode": "en",
+                  "name": "Navigation-41",
+                },
+                Object {
+                  "id": 44,
+                  "localeCode": "ff",
+                  "name": "Navigation-43",
+                },
+              ],
+              "name": "Navigation-47",
+            },
+            Object {
+              "id": 56,
+              "localeCode": "ff",
+              "localizations": Array [
+                Object {
+                  "id": 50,
+                  "localeCode": "en",
+                  "name": "Navigation-49",
+                },
+                Object {
+                  "id": 52,
+                  "localeCode": "pl",
+                  "name": "Navigation-51",
+                },
+              ],
+              "name": "Navigation-55",
+            },
+          ]
+        `);
+      });
+    });
+  });
 });
