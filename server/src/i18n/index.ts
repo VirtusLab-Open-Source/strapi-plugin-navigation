63ae513f566090af832a6d32d5dfe63afe3e7dca
Tomasz Puch <tpuch@virtuslab.com>
2024-09-26 14:53:11 +0200

    fix: fallback main navigation

--- server/src/i18n/index.ts
+++ server/src/i18n/index.ts
@@ -2,6 +2,9 @@ import { Core } from '@strapi/strapi';
 import { getNavigationRepository } from '../repositories';
 import { getPluginService } from '../utils';
 
+const DEFAULT_NAVIGATION_NAME = 'Navigation';
+const DEFAULT_NAVIGATION_SLUG = 'navigation';
+
 export const navigationSetup = async (context: { strapi: Core.Strapi }) => {
   const commonService = getPluginService(context, 'common');
 
@@ -13,6 +16,20 @@ export const navigationSetup = async (context: { strapi: Core.Strapi }) => {
     limit: Number.MAX_SAFE_INTEGER,
     where: {},
   });
+
+  if (navigations.length === 0) {
+    navigations.push(
+      await navigationRepository.save({
+        name: DEFAULT_NAVIGATION_NAME,
+        localeCode: defaultLocale,
+        visible: true,
+        slug: `${DEFAULT_NAVIGATION_SLUG}-${defaultLocale}`,
+      })
+    );
+
+    console.log("");
+    console.log(`Fallback navigation added`);
+  }
 
   const defaultLocaleNavigations = navigations.filter(
     ({ localeCode }) => localeCode === defaultLocale
