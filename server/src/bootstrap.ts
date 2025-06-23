fb71af3249068e7fcae2959949bbb56e713e507f
Mateusz Kleszcz <mkleszcz@virtuslab.com>
2025-06-23 14:53:43 +0200

    fix: update navigation when new locale is created (#575)
    Co-authored-by: matuesz-kleszcz <mateusz.kleszcz@dhl.com>

--- server/src/bootstrap.ts
+++ server/src/bootstrap.ts
@@ -4,6 +4,7 @@ import { configSetup } from './config';
 import { navigationSetup } from './i18n';
 import { setupPermissions } from './permissions';
 import { graphQLSetup } from './graphql';
+import { getPluginService } from './utils';
 
 const bootstrap = async (context: { strapi: Core.Strapi }) => {
   await configSetup(context);
@@ -12,5 +13,13 @@ const bootstrap = async (context: { strapi: Core.Strapi }) => {
   await graphQLSetup(context);
 
   await strapi.service('plugin::navigation.migrate').migrateRelatedIdToDocumentId();
+
+  strapi.db.lifecycles.subscribe({
+    models: ['plugin::i18n.locale'],
+    async afterCreate(event) {
+      const adminService = getPluginService(context, 'admin');
+      await adminService.refreshNavigationLocale(event.result?.code);
+    },
+  });
 };
 export default bootstrap;
