81f449aea86476470a9dd94c4667fee5ef525af2
mateusz-kleszcz <mkleszcz@virtuslab.com>
2025-08-27 09:32:10 +0200

    fix: fix formatting in wrapper file

--- admin/src/pages/HomePage/components/NavigationItemListItem/Wrapper.ts
+++ admin/src/pages/HomePage/components/NavigationItemListItem/Wrapper.ts
@@ -13,34 +13,34 @@ const Wrapper = styled.div.withConfig({
   ${({ level, theme, isLast }) =>
     level &&
     `
-    &::before {
-      ${!isLast && 'content: "";'}
-      display: block;
-      top: ${theme.spaces[1]};
-      left: -24px;
-      position: absolute;
-      height: calc(100% + ${theme.spaces[2]});
-      width: 19px;
-      border: 0px solid transparent;
-      border-left: 4px solid ${theme.colors.neutral300};
-    }
+      &::before {
+        ${!isLast && 'content: "";'}
+        display: block;
+        top: ${theme.spaces[1]};
+        left: -24px;
+        position: absolute;
+        height: calc(100% + ${theme.spaces[2]});
+        width: 19px;
+        border: 0px solid transparent;
+        border-left: 4px solid ${theme.colors.neutral300};
+      }
 
-    &::after {
-      content: "";
-      display: block;
-      height: 22px;
-      width: 19px;
-      position: absolute;
-      top: ${theme.spaces[1]};
-      left: -${theme.spaces[6]};
-      
-      background: transparent;
-      border: 4px solid ${theme.colors.neutral300};
-      border-top: transparent;
-      border-right: transparent;
-      border-radius: 0 0 0 100%;
-    }
-  `};
+      &::after {
+        content: "";
+        display: block;
+        height: 22px;
+        width: 19px;
+        position: absolute;
+        top: ${theme.spaces[1]};
+        left: -${theme.spaces[6]};
+        
+        background: transparent;
+        border: 4px solid ${theme.colors.neutral300};
+        border-top: transparent;
+        border-right: transparent;
+        border-radius: 0 0 0 100%;
+      }
+    `};
 `;
 
 export default Wrapper;
