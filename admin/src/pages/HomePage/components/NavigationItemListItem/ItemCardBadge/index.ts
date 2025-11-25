15c1f6b68b375032513df1b5da38f4584ccaff39
Mateusz Ziarko <mziarko@virtuslab.com>
2025-11-25 10:44:14 +0100

    fix: typings issue

--- admin/src/pages/HomePage/components/NavigationItemListItem/ItemCardBadge/index.ts
+++ admin/src/pages/HomePage/components/NavigationItemListItem/ItemCardBadge/index.ts
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export const ItemCardBadge = styled(Badge).withConfig({
   shouldForwardProp: (prop) => !['small'].includes(prop),
 })`
-  border: 1px solid ${({ theme, borderColor }) => theme.colors[borderColor]};
+  border: 1px solid ${({ theme, borderColor }) => theme.colors[borderColor as keyof typeof theme.colors]};
 
   ${({ small, theme }) =>
     small &&
