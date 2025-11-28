621db24867668b29f4c4ce6b5ad060f4f85dd15f
mateusz-kleszcz <mkleszcz@virtuslab.com>
2025-11-28 09:34:03 +0100

    fix: read default locale

--- server/src/utils/migration.ts
+++ server/src/utils/migration.ts
@@ -3,7 +3,7 @@ import { getNavigationRepository } from "../repositories";
 
 export const removeNavigationsWithoutDefaultLocale = async (context: { strapi: Core.Strapi }) => {
   const allNavigations = await getNavigationRepository(context).find({ locale: '*', limit: Number.MAX_SAFE_INTEGER });
-  const defaultLocale = context.strapi.plugin('i18n').service('locales').getDefaultLocale().code;
+  const defaultLocale = await context.strapi.plugin('i18n').service('locales').getDefaultLocale();
   await Promise.all(allNavigations.map(async (navigation) => {
     const root = allNavigations.find(({ documentId, locale }) => documentId === navigation.documentId && locale === defaultLocale);
     if (!root) {
